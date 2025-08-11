c59536b863d4c446876016ad32bf8f52030b6c1e
João Dorea <84815558+joaopdorea@users.noreply.github.com>
2025-08-11 20:26:41 -0300

    ajustando caminho config

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../config.yaml') as file:
+with open('./config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
