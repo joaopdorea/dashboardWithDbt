ef05047f41722fb2f5540fd8f98e5ba6714859f5
João Dorea <84815558+joaopdorea@users.noreply.github.com>
2025-08-11 20:22:41 -0300

    adicionando autenticação

--- app/app.py
+++ app/app.py
@@ -1,9 +1,29 @@
 import os
 import pandas as pd
 import streamlit as st
+import streamlit_authenticator as stauth
 from sqlalchemy import create_engine
 from sqlalchemy.exc import ProgrammingError
 from dotenv import load_dotenv
+
+import yaml
+from yaml.loader import SafeLoader
+
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Pre-hashing all plain text passwords once
+# stauth.Hasher.hash_passwords(config['credentials'])
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+
+
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -38,18 +58,30 @@ def get_data():
     df = pd.read_sql(query, engine)
     return df
 
-# Configurar a página do Streamlit
-st.set_page_config(page_title='Dashboard do diretor', layout='wide')
 
-# Título do Dashboard
-st.title('Dashboard commodities')
 
-# Descrição
-st.write("""
-Este dashboard mostra os dados de commodities e suas transações.
-""")
+authenticator.login()
 
-# Obter os dados
-df = get_data()
 
-st.dataframe(df)
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    # Configurar a página do Streamlit
+    st.set_page_config(page_title='Dashboard do diretor', layout='wide')
+
+    # Título do Dashboard
+    st.title('Dashboard commodities')
+
+    # Descrição
+    st.write("""
+    Este dashboard mostra os dados de commodities e suas transações.
+    """)
+
+    # Obter os dados
+    df = get_data()
+
+    st.dataframe(df)
+elif st.session_state["authentication_status"] is False:
+    st.error('Usuário/Senha is inválido')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Por Favor, utilize seu usuário e senha!')
+
