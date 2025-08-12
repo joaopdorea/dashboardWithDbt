dd9f1304e762b0b7cdd902e7116dccb8a74c1f9a
João Dorea <84815558+joaopdorea@users.noreply.github.com>
2025-08-11 21:13:35 -0300

    adicionando cartões no dashboard

--- app/app.py
+++ app/app.py
@@ -58,6 +58,36 @@ def get_data():
     df = pd.read_sql(query, engine)
     return df
 
+def get_receita_liquida():
+    query = """
+        SELECT SUM(ganho) AS total_ganho
+        FROM public.silver_commodities;
+    """
+    indicador = pd.read_sql(query, engine)
+    valor = indicador["total_ganho"].iloc[0]
+    return round(valor, 2)
+
+def get_faturamento():
+    query = """
+        SELECT SUM(ganho) AS faturamento
+        FROM public.silver_commodities
+        WHERE tipo_acao = 'sell';
+    """
+    indicador = pd.read_sql(query, engine)
+    valor = indicador["faturamento"].iloc[0]
+    return round(valor, 2)
+
+
+def get_despesas():
+    query = """
+        SELECT SUM(ganho) AS despesa
+        FROM public.silver_commodities
+        WHERE tipo_acao = 'buy';
+    """
+    indicador = pd.read_sql(query, engine)
+    valor = indicador["despesa"].iloc[0]
+    return round(valor, 2)
+
 
 
 authenticator.login()
@@ -79,7 +109,35 @@ if st.session_state["authentication_status"]:
     # Obter os dados
     df = get_data()
 
+    def format_brl(number):
+    # Formata com 2 casas decimais, separador de milhar '.' e decimal ','
+        return f"{number:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+
+    receitaLiquida = format_brl(get_receita_liquida())
+    faturamento = format_brl(get_faturamento())
+    despesa = format_brl(get_despesas())
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric(
+        label="Receita líquida",
+        value="R$ "+ str(receitaLiquida)
+    )
+    
+    with col2:
+        st.metric(
+        label="Faturamento",
+        value="R$ "+ str(faturamento)
+    )
+    
+    with col3:
+        st.metric(
+        label="Despesas",
+        value="R$ "+ str(despesa)
+    )
     st.dataframe(df)
+
+
 elif st.session_state["authentication_status"] is False:
     st.error('Usuário/Senha is inválido')
 elif st.session_state["authentication_status"] is None:
